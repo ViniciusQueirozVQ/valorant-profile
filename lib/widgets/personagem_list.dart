2175bb4dd1dcd3759d58b1d4685fd017ebd7f37e
ViniciusQueirozVQ <viniciusqueiroz.voq@gmail.com>
2023-04-29 21:43:54 -0300

    fix: add gerador de card

--- lib/widgets/personagem_list.dart
+++ lib/widgets/personagem_list.dart
@@ -50,61 +50,7 @@ class _PersonagemListState extends State<PersonagemList> {
             children: [
               ListView.builder(
                   controller: _scrollController,
-                  itemBuilder: ((context, index) {
-                    final personagem = personagemRepository.personagens[index];
-                    return GestureDetector(
-                      child: Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Row(
-                            children: [
-                              Container(
-                                width: 80.0,
-                                height: 80.0,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  image: DecorationImage(
-                                    image: AssetImage(personagem.avatar),
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 10.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      personagem.nome,
-                                      style: const TextStyle(
-                                          fontSize: 22.0,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    Text(
-                                      personagem.classe,
-                                      style: const TextStyle(
-                                          fontSize: 22.0,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    Text(
-                                      personagem.nacionalidade,
-                                      style: const TextStyle(
-                                        fontSize: 22.0,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => DetalhesPage(personagem.id)));
-                      },
-                    );
-                  }),
+                  itemBuilder: ((context, index) => cardBuilder(context, index)),
                   itemCount: personagemRepository.personagens.length),
               loadingIndicatorWidget(),
             ],
@@ -118,23 +64,80 @@ class _PersonagemListState extends State<PersonagemList> {
         builder: (context, bool isLoading, _) {
           return (isLoading)
               ? Positioned(
-                  left: (MediaQuery.of(context).size.width / 2) - 20,
-                  bottom: 24,
-                  child: const SizedBox(
-                    width: 40,
-                    height: 40,
-                    child: CircleAvatar(
-                      child: SizedBox(
-                        width: 20,
-                        height: 20,
-                        child: CircularProgressIndicator(
-                          strokeWidth: 2,
-                        ),
-                      ),
-                    ),
+            left: (MediaQuery.of(context).size.width / 2) - 20,
+            bottom: 24,
+            child: const SizedBox(
+              width: 40,
+              height: 40,
+              child: CircleAvatar(
+                child: SizedBox(
+                  width: 20,
+                  height: 20,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 2,
                   ),
-                )
+                ),
+              ),
+            ),
+          )
               : Container();
         });
   }
+
+  Widget cardBuilder(BuildContext context, int index) {
+    final personagem = personagemRepository.personagens[index];
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            children: [
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage(personagem.avatar),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      personagem.nome,
+                      style: const TextStyle(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      personagem.classe,
+                      style: const TextStyle(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      personagem.nacionalidade,
+                      style: const TextStyle(
+                        fontSize: 22.0,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => DetalhesPage(personagem.id)));
+      },
+    );
+  }
 }
+
